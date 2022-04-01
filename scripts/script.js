51024b07407be6a18ca95c748b3b9c7cc74c804c
Evgeniy Rolenko <jeka@rolenko.ru>
2022-04-02 00:11:02 +0300

    Palych edition

--- scripts/script.js
+++ scripts/script.js
@@ -1,48 +1,40 @@
 const popup = document.querySelector('.popup');
 console.log(popup);
 
-const popupForm = document.querySelector('.popup__form');
-console.log(popupForm);
+const popupProfileForm = document.querySelector('.popup__form');
+console.log(popupProfileForm);
+
+//TODO Add Element from
+// const popupAddElement = document.querySelector('.popup__form');
+// console.log(popupAddElement);
+
 
 const profile = document.querySelector('.profile');
 console.log(profile);
 
-const popupInputProfileTitle = popupForm.querySelector('#popup-profile-title');
-console.log(popupInputProfileTitle);
+const popupProfileInputTitle = popupProfileForm.querySelector('#popup-profile-title');
+console.log(popupProfileInputTitle);
 
-const popupInputProfileSubtitle = popupForm.querySelector('#popup-profile-subtitle');
-console.log(popupInputProfileSubtitle);
+const popupProfileInputSubtitle = popupProfileForm.querySelector('#popup-profile-subtitle');
+console.log(popupProfileInputSubtitle);
 
-const popupProfileCloseButton = popupForm.querySelector('.popup__btn-close');
+const popupProfileCloseButton = popupProfileForm.querySelector('.popup__btn-close');
 console.log(popupProfileCloseButton);
 
-const profileSaveButton = popupForm.querySelector('.popup__btn-account-submit');
-console.log(profileSaveButton);
+const popupProfileSaveButton = popupProfileForm.querySelector('.popup__btn-account-submit');
+console.log(popupProfileSaveButton);
 
-const popupProfileEditButton = profile.querySelector('.profile__btn-edit');
-console.log(popupProfileEditButton);
+const ProfileEditButton = profile.querySelector('.profile__btn-edit');
+console.log(ProfileEditButton);
 
 
-azaza = popupInputProfileSubtitle.hasAttribute('disabled');
-console.log(azaza);
 
-if (azaza) {
-  popup.classList.remove('popup_opened');
-} else {
-  console.log('выкл');
-}
+let toggleEditProfileForm = () => popup.classList.toggle('popup_opened');
 
-popupProfileCloseButton.addEventListener('click', closeEditProfileForm);
-popupProfileEditButton.addEventListener('click', openEditProfileForm);
+popupProfileCloseButton.addEventListener('click', toggleEditProfileForm);
+ProfileEditButton.addEventListener('click', toggleEditProfileForm);
 
-function closeEditProfileForm() {
-  popup.classList.remove('popup_opened');
-}
 
-function openEditProfileForm() {
-  popup.classList.add('popup_opened');
-}
-
-// const element = document.querySelector('.element');
-// const elementButtonLike = element.querySelector('.element__btn-like');
-// if (elementButtonLike.classList.contains('element__btn-like'))
+popupProfileForm.addEventListener('submit',  function () {
+  console.log('test')
+} );
