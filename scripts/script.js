1811304457c91d841368b4656ae3d7ab0d24252a
Evgeniy Rolenko <jeka@rolenko.ru>
2022-04-19 11:47:06 +0300

    hotfixex

--- scripts/script.js
+++ scripts/script.js
@@ -36,9 +36,13 @@ const profilePopupInputSubtitle = profilePopupForm.querySelector('#popup-profile
 const profilePopupButtonClose = profilePopupForm.querySelector('#profile-btn-close');
 
 // element popup
+
 const elementPopup = document.querySelector('#popup-element');
 const elementPopupButtonOpen = document.querySelector('.profile__btn-add');
 const elementPopupButtonClose = document.querySelector('#card-btn-close');
+const elementName = document.querySelector('#element-name');
+const elementLink = document.querySelector('#element-url');
+const elementTrash = document.querySelector('.element__trash')
 
 // profile
 const profile = document.querySelector('.profile');
@@ -59,19 +63,19 @@ const elementsList = document.querySelector('.elements__list');
 const elementTemplate = document.querySelector('#element-template').content;
 
 //2. Functions
-let openPopup = (popup) => popup.classList.add('popup_opened'); // open edit profile popup
-let closePopup = (popup) => popup.classList.remove('popup_opened'); // close edit profile popup
-let closePopupOutsideClick = function (evt) { // close any popup over outside click
+const openPopup = (popup) => popup.classList.add('popup_opened'); // open edit profile popup
+const closePopup = (popup) => popup.classList.remove('popup_opened'); // close edit profile popup
+const closePopupOutsideClick = function (evt) { // close any popup over outside click
   if (evt.target === evt.currentTarget) {
     closePopup(evt.target);
   }
 }
-let loadEditProfileForm = function (popup){ // open edit profile popup
+const loadEditProfileForm = function (popup){ // open edit profile popup
   profilePopupInputTitle.value = profileTitleValue.textContent;
   profilePopupInputSubtitle.value = profileSubtitleValue.textContent;
   openPopup(popup);
 }
-let saveEditProfileForm = function (evt) { // Save edit profile form
+const saveEditProfileForm = function (evt) { // Save edit profile form
   evt.preventDefault();
   profileTitleValue.textContent = profilePopupInputTitle.value;
   profileSubtitleValue.textContent = profilePopupInputSubtitle.value;
@@ -81,19 +85,24 @@ function loadElements(elementName, elementLink) { // create element node
   const element = elementTemplate.querySelector('.element').cloneNode(true);
   const likeBtn = element.querySelector('.element__btn-like');
   const elementImage = element.querySelector('.element__image');
-  element.querySelector('.element__name').textContent = elementName;
-  element.querySelector('.element__image').src = elementLink;
-  element.querySelector('.element__image').alt = elementName;
-  likeBtn.addEventListener("click", () => likeBtn.classList.toggle('element__btn-like_active'));
+  const elementTrash = element.querySelector('.element__trash');
 
+  element.querySelector('.element__name').textContent = elementName;
+  elementImage.src = elementLink;
+  elementImage.alt = elementName;
+  likeBtn.addEventListener("click", () => likeBtn.classList.toggle('element__btn-like_active'));
 
   elementImage.addEventListener('click', () => { //add preview action for new elements from array
     openPopupImage(elementLink, elementName)
   });
 
-  element.querySelector('.element__trash').addEventListener('click', function (evt) {
+  elementTrash.addEventListener('click', function (evt) {
     evt.target.closest('.element').remove();
   });
+  return element;
+}
+
+function renderElement(element) { //render elements
   elementsList.prepend(element);
 }
 
@@ -101,30 +110,50 @@ function openPopupImage(imageLink, imageTitle) { //open preview image popup
   openPopup(previewPopup);
   previewImage.setAttribute('src', imageLink);
   previewTitle.textContent = imageTitle;
+  previewImage.setAttribute('alt', imageTitle);
 }
 
 function createElement(evt) { //add image from popup
   evt.preventDefault();
-  const elementName = document.querySelector('#element-name');
-  const elementLink = document.querySelector('#element-url');
-  loadElements(elementName.value, elementLink.value);
+  renderElement(loadElements(elementName.value, elementLink.value));
   elementName.value = '';
   elementLink.value = '';
   closePopup(elementPopup);
 }
 
 //3. Global actions
-profilePopupButtonClose.addEventListener('click', ()=> {closePopup(profilePopup)});
-profileButtonEdit.addEventListener('click', ()=> {loadEditProfileForm(profilePopup)});
+profilePopupButtonClose.addEventListener('click', ()=> {
+  closePopup(profilePopup)
+});
+
+profileButtonEdit.addEventListener('click', ()=> {
+  loadEditProfileForm(profilePopup)
+});
+
 profilePopup.addEventListener('click', closePopupOutsideClick);
+
 profilePopupForm.addEventListener('submit', saveEditProfileForm);
-elementPopupButtonClose.addEventListener('click', ()=> {closePopup(elementPopup)});
-elementPopupButtonOpen.addEventListener('click', ()=> {openPopup(elementPopup)});
+
+elementPopupButtonClose.addEventListener('click', ()=> {
+  closePopup(elementPopup)
+});
+
+elementPopupButtonOpen.addEventListener('click', ()=> {
+  openPopup(elementPopup)
+});
+
 elementPopup.addEventListener('click', closePopupOutsideClick);
+
 elementPopup.addEventListener('submit', createElement);
+
 previewPopup.addEventListener('click', closePopupOutsideClick);
-previewPopupButtonClose.addEventListener('click', ()=> {closePopup(previewPopup)})
-initialCards.reverse().forEach(element => loadElements(element.name, element.link));
+
+previewPopupButtonClose.addEventListener('click', ()=> {
+  closePopup(previewPopup)
+})
+initialCards.reverse().forEach((element) => {
+  renderElement(loadElements(element.name, element.link))
+});
 
 
 
