64bfc4051fe945e50f2727739aa13a7cde877c6f
Evgeniy Rolenko <jeka@rolenko.ru>
2022-11-14 00:12:06 +0300

    Create Api class

--- src/components/api.js
+++ src/components/api.js
@@ -1,100 +1,194 @@
-//import vars from constants
-import {
-  apiURL,
-  headers,
-} from "./const.js";
-
-// Request
-function request(url, options) {
-  return fetch(url, options)
-    .then(checkResponse)
-}
-
-// Response
-function checkResponse(res) {
-  if (res.ok) {
-    return res.json();
+export default class Api {
+  constructor(url, headers) {
+    this._url = url;
+    this._headers = headers;
   }
-  return Promise.reject(res);
-}
 
-// GET profile
-function getProfile() {
-  return request(`${apiURL}/users/me`, {
-    headers
-  })
-}
+  // Request
+  _request(url, options) {
+    return fetch(url, options).then(this._checkResponse)
+  }
 
-// GET cards
-function getCards() {
-  return request(`${apiURL}/cards`, {
-    headers
-  })
-}
+  // Response
+  _checkResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(res);
+  }
+
+  // GET profile
+  _getProfile() {
+    return this._request(`${this._url}/users/me`, {
+      headers: this._headers
+    })
+  }
+
+  // GET cards
+  _getCards() {
+    return this._request(`${this._url}/cards`, {
+      headers: this._headers
+    })
+  }
 
 // ADD CARD request
-function addCard(body) {
-  return request(`${apiURL}/cards`, {
-    method: 'POST',
-    headers,
-    body: JSON.stringify(body)
-  })
-}
+  _addCard(body) {
+    return this._request(`${this._url}/cards`, {
+      method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify(body)
+    })
+  }
 
 // DELETE CARD request
-function deleteCard(id) {
-  return request(`${apiURL}/cards/${id}`, {
-    method: 'DELETE',
-    headers
-  })
-}
+  _deleteCard(id) {
+    return this._request(`${this._url}/cards/${id}`, {
+      method: 'DELETE',
+      headers: this._headers
+    })
+  }
 
 // PATCH PROFILE DATA request
-function patchProfile(name, about) {
-  return request(`${apiURL}/users/me`, {
-    method: 'PATCH',
-    headers,
-    body: JSON.stringify({
-      name: name,
-      about: about
+  _patchProfile(name, about) {
+    return this._request(`${this._url}/users/me`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        name: name,
+        about: about
+      })
     })
-  })
-}
+  }
 
 // PATCH PROFILE AVATAR request
-function patchAvatar(link) {
-  return request(`${apiURL}/users/me/avatar`, {
-    method: 'PATCH',
-    headers,
-    body: JSON.stringify({
-      avatar: link,
+  _patchAvatar(link) {
+    return this._request(`${this._url}/users/me/avatar`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        avatar: link,
+      })
     })
-  })
-}
+  }
 
 // PUT LIKE request
-function putLike(id) {
-  return request(`${apiURL}/cards/likes/${id}`, {
-    method: 'PUT',
-    headers
-  })
-}
+  _putLike(id) {
+    return this._request(`${this._url}/cards/likes/${id}`, {
+      method: 'PUT',
+      headers: this._headers
+    })
+  }
 
 // DELETE LIKE request
-function deleteLike(id) {
-  return request(`${apiURL}/cards/likes/${id}`, {
-    method: 'DELETE',
-    headers
-  })
+  _deleteLike(id) {
+    return this._request(`${this._url}/cards/likes/${id}`, {
+      method: 'DELETE',
+      headers: this._headers
+    })
+  }
+
+
 }
 
-export {
-  addCard,
-  deleteCard,
-  patchProfile,
-  patchAvatar,
-  putLike,
-  deleteLike,
-  getProfile,
-  getCards
-}
+// +++++++++++++++ OLD  CODE +++++++++++++++
+// ++++++++++++++ DONT DELETE ++++++++++++++
+// //import vars from constants
+// import {
+//   apiURL,
+//   headers,
+// } from "./const.js";
+//
+// // Request
+// function request(url, options) {
+//   return fetch(url, options)
+//     .then(checkResponse)
+// }
+//
+// // Response
+// function checkResponse(res) {
+//   if (res.ok) {
+//     return res.json();
+//   }
+//   return Promise.reject(res);
+// }
+//
+// // GET profile
+// function getProfile() {
+//   return request(`${apiURL}/users/me`, {
+//     headers
+//   })
+// }
+//
+// // GET cards
+// function getCards() {
+//   return request(`${apiURL}/cards`, {
+//     headers
+//   })
+// }
+//
+// // ADD CARD request
+// function addCard(body) {
+//   return request(`${apiURL}/cards`, {
+//     method: 'POST',
+//     headers,
+//     body: JSON.stringify(body)
+//   })
+// }
+//
+// // DELETE CARD request
+// function deleteCard(id) {
+//   return request(`${apiURL}/cards/${id}`, {
+//     method: 'DELETE',
+//     headers
+//   })
+// }
+//
+// // PATCH PROFILE DATA request
+// function patchProfile(name, about) {
+//   return request(`${apiURL}/users/me`, {
+//     method: 'PATCH',
+//     headers,
+//     body: JSON.stringify({
+//       name: name,
+//       about: about
+//     })
+//   })
+// }
+//
+// // PATCH PROFILE AVATAR request
+// function patchAvatar(link) {
+//   return request(`${apiURL}/users/me/avatar`, {
+//     method: 'PATCH',
+//     headers,
+//     body: JSON.stringify({
+//       avatar: link,
+//     })
+//   })
+// }
+//
+// // PUT LIKE request
+// function putLike(id) {
+//   return request(`${apiURL}/cards/likes/${id}`, {
+//     method: 'PUT',
+//     headers
+//   })
+// }
+//
+// // DELETE LIKE request
+// function deleteLike(id) {
+//   return request(`${apiURL}/cards/likes/${id}`, {
+//     method: 'DELETE',
+//     headers
+//   })
+// }
+//
+// export {
+//   addCard,
+//   deleteCard,
+//   patchProfile,
+//   patchAvatar,
+//   putLike,
+//   deleteLike,
+//   getProfile,
+//   getCards
+// }
