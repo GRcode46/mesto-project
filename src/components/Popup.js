d3b92cb24e4d359aa18a5772a5680bf05dab3179
Avet90 <avet.sarkisian90@icloud.com>
2022-11-20 22:45:28 +0300

    add Popup

---
+++ src/components/Popup.js
@@ -0,0 +1,44 @@
+import {selectors} from "../vendor/const";
+
+export default class Popup {
+  static classes = {
+    popupClass: "popup",
+    popupOpened: "popup_opened",
+  };
+
+  constructor(popupSelector) {
+    this._popup = document.querySelector(popupSelector);
+    this._closePopupEsc = this._closePopupEsc.bind(this);
+    this._closePopupOverlay = this._closePopupOverlay.bind(this);
+  }
+
+  openPopup() {
+    this._popup.classList.add(Popup.classes.popupOpened);
+    document.addEventListener("keydown", this._closePopupEsc);
+    this._popup.addEventListener("mousedown", this._closePopupOverlay);
+  }
+
+  closePopup() {
+    this._popup.classList.remove(Popup.classes.popupOpened);
+    document.removeEventListener("keydown", this._closePopupEsc);
+    this._popup.removeEventListener("mousedown", this._closePopupOverlay);
+  }
+
+  _closePopupEsc(evt) {
+    if (evt.key === "Escape") {
+      this.closePopup();
+    }
+  }
+
+  _closePopupOverlay(evt) {
+    if (evt.target.classList.contains(Popup.classes.popupClass)) {
+      this.closePopup();
+    }
+  }
+
+  setEventListeners() {
+    this._popup
+      .querySelector(selectors.popupCloseButton)
+      .addEventListener("click", () => this.closePopup());
+  }
+}
